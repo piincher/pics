b99a26c4f1e4dedd91c165f07e46e661319e7297
Pro <pro@Pros-MacBook-Pro.local>
2020-09-17 13:53:34 +0800

    fix the issue a about this in our application

--- src/components/SearchBar.component.jsx
+++ src/components/SearchBar.component.jsx
@@ -7,11 +7,15 @@ class SearchBar extends React.Component {
 			term: ''
 		};
 	}
+	onFormSubmit = (event) => {
+		event.preventDefault();
+		this.props.onSubmit(this.state.term);
+	};
 
 	render() {
 		return (
 			<div className="ui segment">
-				<form className="ui form">
+				<form onSubmit={this.onFormSubmit} className="ui form">
 					<div className="field">
 						<label>search Image</label>
 						<input
